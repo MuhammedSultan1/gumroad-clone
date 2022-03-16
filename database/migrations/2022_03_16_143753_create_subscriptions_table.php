d5f9486d96e3298bf9253d70246c3ad4bc13f5e9
MuhammedSultan1 <mfsultan01@gmail.com>
2022-03-16 12:51:23 -0400

    added columns

--- database/migrations/2022_03_16_143753_create_subscriptions_table.php
+++ database/migrations/2022_03_16_143753_create_subscriptions_table.php
@@ -14,8 +14,21 @@ return new class extends Migration
     public function up()
     {
         Schema::create('subscriptions', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('plan_id');
+            $table->string('name');
+            $table->string('stripe_id')->unique();
+            $table->string('stripe_status');
+            $table->string('stripe_price')->nullable();
+            $table->integer('quantity')->nullable();
+            $table->timestamp('trial_ends_at')->nullable();
+            $table->timestamp('ends_at')->nullable();
+            $table->index(['user_id', 'stripe_status']);
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('plan_id')->references('id')->on('membership_plans')->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
