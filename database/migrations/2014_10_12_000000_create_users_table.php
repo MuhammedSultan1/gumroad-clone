b560349c144431930b0415639945e6704e4224bb
MuhammedSultan1 <mfsultan01@gmail.com>
2022-03-16 08:05:55 -0400

    added first name, last name, user type and profile image

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->smallInteger('user_type')->default('1');
+            $table->string('profile_image')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
