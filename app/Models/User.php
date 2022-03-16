eb56b824099c8d2d11fc8b68e79b4d34dcffef8f
MuhammedSultan1 <mfsultan01@gmail.com>
2022-03-16 08:06:16 -0400

    added getters and constants

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,15 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    const DEFAULT = 1;
+    const MERCHANT = 2;
+    const ADMIN = 3;
+
+    const TABLE = 'users';
+
+    protected $table = self::TABLE;
+
+
     /**
      * The attributes that are mass assignable.
      *
@@ -21,6 +30,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'type',
     ];
 
     /**
@@ -41,4 +51,36 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    // Methods
+
+    public function getID(): int
+    {
+        return $this->id;
+    }
+
+    public function getFirstName(): string
+    {
+        return $this->first_name;
+    }
+
+    public function getLastName(): string
+    {
+        return $this->last_name;
+    }
+
+    public function getUserType(): int
+    {
+        return $this->type;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function getProfileImage(): string
+    {
+        return $this->profile_image;
+    }
 }
