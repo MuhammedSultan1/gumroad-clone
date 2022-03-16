a0a328e43c5189c0f387eaae422a53cfd55bd546
MuhammedSultan1 <mfsultan01@gmail.com>
2022-03-16 08:15:16 -0400

    removed code that wasn't necessary

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
@@ -16,59 +15,5 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        User::factory()->count(1)->create([
-            'name' => 'Admin User',
-            'email' => 'admin@example.com',
-            'password' => bcrypt('password'),
-            'type' => User::ADMIN,
-        ]);
-        // Merchant Users
-        User::factory()->create([
-            'name'          => 'Avery User',
-            'email'         => 'Avery@example.com',
-            'password'      => bcrypt('password'),
-            'type'          => User::MERCHANT,
-        ]);
-
-        User::factory()->create([
-            'name'          => 'Hannah User',
-            'email'         => 'Hannah@example.com',
-            'password'      => bcrypt('password'),
-            'type'          => User::MERCHANT,
-        ]);
-
-        User::factory()->create([
-            'name'          => 'Fred User',
-            'email'         => 'Fred@example.com',
-            'password'      => bcrypt('password'),
-            'type'          => User::MERCHANT,
-        ]);
-
-        // Default Users
-        User::factory()->create([
-            'name'          => 'Max Doe',
-            'email'         => 'Max@example.com',
-            'password'      => bcrypt('password'),
-            'type'          => User::DEFAULT,
-        ]);
-
-        User::factory()->create([
-            'name'          => 'steve Doe',
-            'email'         => 'steve@example.com',
-            'password'      => bcrypt('password'),
-            'type'          => User::DEFAULT,
-        ]);
-        User::factory()->create([
-            'name'          => 'Jeff Doe',
-            'email'         => 'Jeff@example.com',
-            'password'      => bcrypt('password'),
-            'type'          => User::DEFAULT,
-        ]);
-        User::factory()->create([
-            'name'          => 'patrick Doe',
-            'email'         => 'patrick@example.com',
-            'password'      => bcrypt('password'),
-            'type'          => User::DEFAULT,
-        ]);
     }
 }
