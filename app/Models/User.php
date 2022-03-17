621b7f79148ff5a1b44ecc02c8df233691a664fa
MuhammedSultan1 <mfsultan01@gmail.com>
2022-03-17 07:28:14 -0400

    added billable trait

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,16 @@
 
 namespace App\Models;
 
+use Laravel\Cashier\Billable;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, Billable;
 
     const DEFAULT = 1;
     const MERCHANT = 2;
