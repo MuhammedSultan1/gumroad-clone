c6c45950db0800b55903afcc285d1d14c62982fc
MuhammedSultan1 <mfsultan01@gmail.com>
2022-03-18 08:44:56 -0400

    takes first name and last name

--- app/Models/User.php
+++ app/Models/User.php
@@ -28,7 +28,8 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
         'email',
         'password',
         'type',
