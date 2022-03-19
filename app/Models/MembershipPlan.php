6af258ed221cee5da3eb642f8909d7e6f1a2e727
MuhammedSultan1 <mfsultan01@gmail.com>
2022-03-19 15:30:03 -0400

    added getters

--- app/Models/MembershipPlan.php
+++ app/Models/MembershipPlan.php
@@ -8,4 +8,40 @@ use Illuminate\Database\Eloquent\Model;
 class MembershipPlan extends Model
 {
     use HasFactory;
+
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getPrice(): int
+    {
+        return $this->price;
+    }
+
+    public function getBenefitOne(): string
+    {
+        return $this->benefit_one;
+    }
+
+    public function getBenefitTwo(): string
+    {
+        return $this->benefit_two;
+    }
+
+    public function getBenefitThree(): string
+    {
+        return $this->benefit_three;
+    }
+
+    public function getBenefitFour(): string
+    {
+        return $this->benefit_four;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
 }
