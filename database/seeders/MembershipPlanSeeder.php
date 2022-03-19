b8ae1dbe8cdbbb602fbb3637164275515e93d65f
MuhammedSultan1 <mfsultan01@gmail.com>
2022-03-19 04:32:54 -0400

    added data for seeding

--- database/seeders/MembershipPlanSeeder.php
+++ database/seeders/MembershipPlanSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\MembershipPlan;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class MembershipPlanSeeder extends Seeder
 {
@@ -14,6 +15,23 @@ class MembershipPlanSeeder extends Seeder
      */
     public function run()
     {
-        //
+        MembershipPlan::create([
+            'name' => 'Basic',
+            'slug' => 'basic-plan',
+            'description' => 'Start your business and build a beautiful website to display your products.',
+            'stripe_name' => 'Basic Plan',
+            'stripe_product_id' => 'prod_L9UeaYK01GrQg7',
+            'stripe_price_id' => 'price_1KTBexCdtNxrS9O3iEtdhaxY',
+            'price' => 20,
+        ]);
+        MembershipPlan::create([
+            'name' => 'Pro',
+            'slug' => 'pro-plan',
+            'description' => 'Everything in the Basic plan and more!',
+            'stripe_name' => 'Pro Plan',
+            'stripe_product_id' => 'prod_L9Ue2YT4RQnZax',
+            'stripe_price_id' => 'price_1KTBexCdtNxrS9O3iEtdhaxY',
+            'price' => 50,
+        ]);
     }
 }
