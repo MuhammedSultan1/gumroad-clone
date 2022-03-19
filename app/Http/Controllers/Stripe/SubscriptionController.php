be451f4589a80f904ba6676493aa0b9d70135adb
MuhammedSultan1 <mfsultan01@gmail.com>
2022-03-19 18:25:29 -0400

    the subscription controller takes the request, updates the user's info and redirects them

--- app/Http/Controllers/Stripe/SubscriptionController.php
+++ app/Http/Controllers/Stripe/SubscriptionController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\Stripe;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\MembershipPlan;
+use App\Http\Controllers\Controller;
 
 class SubscriptionController extends Controller
 {
@@ -37,7 +38,29 @@ class SubscriptionController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $user = $request->user();
+
+        $paymentMethod = $request->input('payment-method');
+
+        $user->update([
+            'line1'         => $request->line1,
+            'line2'         => $request->line2,
+            'city'          => $request->city,
+            'state'         => $request->state,
+            'country'       => $request->country,
+            'postal_code'   => $request->postal_code,
+        ]);
+
+        $plan = MembershipPlan::where('stripe_name', $request->plan)->first();
+
+        $user->newSubscription($plan->stripe_name, $plan->stripe_price_id)
+            ->create($paymentMethod);
+
+        $user->update([
+            'trial_ends_at' => NULL,
+        ]);
+
+        return redirect()->route('billing')->with('success', 'Thank you for subscribing!');
     }
 
     /**
