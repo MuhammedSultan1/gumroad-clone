f8baa2f81899d181f68b75c3b7e14692fb39ea30
MuhammedSultan1 <mfsultan01@gmail.com>
2022-03-19 15:30:37 -0400

    the membership plans and their details now come from the database

--- resources/views/pages/memberships/index.blade.php
+++ resources/views/pages/memberships/index.blade.php
@@ -23,24 +23,26 @@
       pricing. Pay as you grow.
     </h2>
     <p class="text-base text-gray-700 md:text-lg">
-      Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque rem aperiam, eaque ipsa quae.
+        At DigitalCart, we tailor our prices according to the needs of your business.
     </p>
   </div>
   <div class="grid max-w-md gap-10 row-gap-5 lg:max-w-screen-lg sm:row-gap-10 lg:grid-cols-3 xl:max-w-screen-lg sm:mx-auto">
-     <div>
+    @foreach ($memberships as $membership)
+    <div>
         <div class="p-8 bg-gray-900 rounded">
           <div class="mb-4 text-center">
-            <p class="text-xl font-medium tracking-wide text-white">
-              Starter Plan
+            <p class="text-xl font-medium tracking-wide text-teal-400">
+                {{ $membership->getName() }}
             </p>
             <div class="flex items-center justify-center">
               <p class="mr-2 text-5xl font-semibold text-white lg:text-6xl">
-                $39
+                ${{ $membership->getPrice() }}
               </p>
               <p class="text-lg text-gray-500">/ month</p>
             </div>
           </div>
           <ul class="mb-8 space-y-2">
+
             <li class="flex items-center">
               <div class="mr-3">
                 <svg class="w-4 h-4 text-teal-400" viewBox="0 0 24 24" stroke-linecap="round" stroke-width="2">
@@ -48,7 +50,7 @@
                   <circle cx="12" cy="12" fill="none" r="11" stroke="currentColor"></circle>
                 </svg>
               </div>
-              <p class="font-medium text-gray-300">10 deploys per day</p>
+              <p class="font-medium text-gray-300"> {{ $membership->getBenefitOne() }}</p>
             </li>
             <li class="flex items-center">
               <div class="mr-3">
@@ -57,7 +59,7 @@
                   <circle cx="12" cy="12" fill="none" r="11" stroke="currentColor"></circle>
                 </svg>
               </div>
-              <p class="font-medium text-gray-300">10 GB of storage</p>
+              <p class="font-medium text-gray-300"> {{ $membership->getBenefitTwo() }}</p>
             </li>
             <li class="flex items-center">
               <div class="mr-3">
@@ -66,7 +68,7 @@
                   <circle cx="12" cy="12" fill="none" r="11" stroke="currentColor"></circle>
                 </svg>
               </div>
-              <p class="font-medium text-gray-300">3 domains</p>
+              <p class="font-medium text-gray-300"> {{ $membership->getBenefitThree() }}</p>
             </li>
             <li class="flex items-center">
               <div class="mr-3">
@@ -75,137 +77,22 @@
                   <circle cx="12" cy="12" fill="none" r="11" stroke="currentColor"></circle>
                 </svg>
               </div>
-              <p class="font-medium text-gray-300">SSL Certificates</p>
+              <p class="font-medium text-gray-300"> {{ $membership->getBenefitFour() }}</p>
             </li>
+
           </ul>
              <a href="#"
-            class="bg-purple-600 inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md hover:bg-purple-700 focus:shadow-outline focus:outline-none"
+            class="bg-teal-500 inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md hover:bg-teal-600 focus:shadow-outline focus:outline-none"
           >
             Get Now
-        </a>
+            </a>
         </div>
         <div class="w-11/12 h-2 mx-auto bg-gray-900 rounded-b opacity-75"></div>
         <div class="w-10/12 h-2 mx-auto bg-gray-900 rounded-b opacity-50"></div>
         <div class="w-9/12 h-2 mx-auto bg-gray-900 rounded-b opacity-25"></div>
       </div>
-      <div>
-        <div class="p-8 bg-gray-900 rounded">
-          <div class="mb-4 text-center">
-            <p class="text-xl font-medium tracking-wide text-white">Pro Plan</p>
-            <div class="flex items-center justify-center">
-              <p class="mr-2 text-5xl font-semibold text-white lg:text-6xl">
-                $59
-              </p>
-              <p class="text-lg text-gray-500">/ month</p>
-            </div>
-          </div>
-          <ul class="mb-8 space-y-2">
-            <li class="flex items-center">
-              <div class="mr-3">
-                <svg class="w-4 h-4 text-teal-400" viewBox="0 0 24 24" stroke-linecap="round" stroke-width="2">
-                  <polyline fill="none" stroke="currentColor" points="6,12 10,16 18,8"></polyline>
-                  <circle cx="12" cy="12" fill="none" r="11" stroke="currentColor"></circle>
-                </svg>
-              </div>
-              <p class="font-medium text-gray-300">100 deploys per day</p>
-            </li>
-            <li class="flex items-center">
-              <div class="mr-3">
-                <svg class="w-4 h-4 text-teal-400" viewBox="0 0 24 24" stroke-linecap="round" stroke-width="2">
-                  <polyline fill="none" stroke="currentColor" points="6,12 10,16 18,8"></polyline>
-                  <circle cx="12" cy="12" fill="none" r="11" stroke="currentColor"></circle>
-                </svg>
-              </div>
-              <p class="font-medium text-gray-300">50 GB of storage</p>
-            </li>
-            <li class="flex items-center">
-              <div class="mr-3">
-                <svg class="w-4 h-4 text-teal-400" viewBox="0 0 24 24" stroke-linecap="round" stroke-width="2">
-                  <polyline fill="none" stroke="currentColor" points="6,12 10,16 18,8"></polyline>
-                  <circle cx="12" cy="12" fill="none" r="11" stroke="currentColor"></circle>
-                </svg>
-              </div>
-              <p class="font-medium text-gray-300">Unlimited domains</p>
-            </li>
-            <li class="flex items-center">
-              <div class="mr-3">
-                <svg class="w-4 h-4 text-teal-400" viewBox="0 0 24 24" stroke-linecap="round" stroke-width="2">
-                  <polyline fill="none" stroke="currentColor" points="6,12 10,16 18,8"></polyline>
-                  <circle cx="12" cy="12" fill="none" r="11" stroke="currentColor"></circle>
-                </svg>
-              </div>
-              <p class="font-medium text-gray-300">SSL Certificates</p>
-            </li>
-          </ul>
-         <a href="#"
-            class="inline-flex items-center justify-center w-full h-12 px-6 font-semibold tracking-wide text-teal-900 transition duration-200 rounded shadow-md bg-teal-400 hover:bg-teal-700 focus:shadow-outline focus:outline-none"
-          >
-            Get Now
-           </a>
-        </div>
-        <div class="w-11/12 h-2 mx-auto bg-gray-900 rounded-b opacity-75"></div>
-        <div class="w-10/12 h-2 mx-auto bg-gray-900 rounded-b opacity-50"></div>
-        <div class="w-9/12 h-2 mx-auto bg-gray-900 rounded-b opacity-25"></div>
-      </div>
-       <div>
-        <div class="p-8 bg-gray-900 rounded">
-          <div class="mb-4 text-center">
-            <p class="text-xl font-medium tracking-wide text-white">Pro Plan</p>
-            <div class="flex items-center justify-center">
-              <p class="mr-2 text-5xl font-semibold text-white lg:text-6xl">
-                $59
-              </p>
-              <p class="text-lg text-gray-500">/ month</p>
-            </div>
-          </div>
-          <ul class="mb-8 space-y-2">
-            <li class="flex items-center">
-              <div class="mr-3">
-                <svg class="w-4 h-4 text-teal-400" viewBox="0 0 24 24" stroke-linecap="round" stroke-width="2">
-                  <polyline fill="none" stroke="currentColor" points="6,12 10,16 18,8"></polyline>
-                  <circle cx="12" cy="12" fill="none" r="11" stroke="currentColor"></circle>
-                </svg>
-              </div>
-              <p class="font-medium text-gray-300">100 deploys per day</p>
-            </li>
-            <li class="flex items-center">
-              <div class="mr-3">
-                <svg class="w-4 h-4 text-teal-400" viewBox="0 0 24 24" stroke-linecap="round" stroke-width="2">
-                  <polyline fill="none" stroke="currentColor" points="6,12 10,16 18,8"></polyline>
-                  <circle cx="12" cy="12" fill="none" r="11" stroke="currentColor"></circle>
-                </svg>
-              </div>
-              <p class="font-medium text-gray-300">50 GB of storage</p>
-            </li>
-            <li class="flex items-center">
-              <div class="mr-3">
-                <svg class="w-4 h-4 text-teal-400" viewBox="0 0 24 24" stroke-linecap="round" stroke-width="2">
-                  <polyline fill="none" stroke="currentColor" points="6,12 10,16 18,8"></polyline>
-                  <circle cx="12" cy="12" fill="none" r="11" stroke="currentColor"></circle>
-                </svg>
-              </div>
-              <p class="font-medium text-gray-300">Unlimited domains</p>
-            </li>
-            <li class="flex items-center">
-              <div class="mr-3">
-                <svg class="w-4 h-4 text-teal-400" viewBox="0 0 24 24" stroke-linecap="round" stroke-width="2">
-                  <polyline fill="none" stroke="currentColor" points="6,12 10,16 18,8"></polyline>
-                  <circle cx="12" cy="12" fill="none" r="11" stroke="currentColor"></circle>
-                </svg>
-              </div>
-              <p class="font-medium text-gray-300">SSL Certificates</p>
-            </li>
-          </ul>
-          <a href="#"
-            class="inline-flex items-center justify-center w-full h-12 px-6 font-semibold tracking-wide text-teal-900 transition duration-200 rounded shadow-md bg-teal-400 hover:bg-teal-700 focus:shadow-outline focus:outline-none"
-          >
-            Get Now
-        </a>
-        </div>
-        <div class="w-11/12 h-2 mx-auto bg-gray-900 rounded-b opacity-75"></div>
-        <div class="w-10/12 h-2 mx-auto bg-gray-900 rounded-b opacity-50"></div>
-        <div class="w-9/12 h-2 mx-auto bg-gray-900 rounded-b opacity-25"></div>
-      </div>
+    @endforeach
+
   </div>
 </div>
 @endsection
