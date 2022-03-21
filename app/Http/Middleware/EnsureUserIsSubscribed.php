5b1b669ccffdd3ef482de3cbb11ff278a8ee9435
MuhammedSultan1 <mfsultan01@gmail.com>
2022-03-21 12:50:50 -0400

    added middleware: "EnsureUserIsSubscribed"

---
+++ app/Http/Middleware/EnsureUserIsSubscribed.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class EnsureUserIsSubscribed
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($request->user() && !$request->user()->subscribed('Basic Plan' || 'Pro Plan' || 'Enterprise Plan')) {
+            // This user is not a paying customer...
+            return redirect('memberships');
+        }
+
+        return $next($request);
+    }
+}
