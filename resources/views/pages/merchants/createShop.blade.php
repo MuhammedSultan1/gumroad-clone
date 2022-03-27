be926cb532319dc524f77ace9759dc478da03399
MuhammedSultan1 <mfsultan01@gmail.com>
2022-03-27 06:28:05 -0400

    added more fields

--- resources/views/pages/merchants/createShop.blade.php
+++ resources/views/pages/merchants/createShop.blade.php
@@ -23,26 +23,53 @@
           placeholder="Last Name"
       />
     </div>
-    <div class="">
+    <div class="flex">
         <input
             type="text"
             name="shop_name"
             class="px-4 py-3 mt-4 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm"
-            placeholder="Shop Name">
-        </input>
-    </div>
-    <div class="flex">
-        <input
-            type="text"
-            name="user_name"
-            class="px-4 py-3 mt-4 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm"
-            placeholder="User Name"
+            placeholder="Shop Name"
         >
        <span class="px-4 py-3 mt-4 w-1/3 rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm">.digitalcart.com</span>
     </div>
-      <button
+<label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
+<textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-100 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Tell us about yourself or what you're selling"></textarea>
+    <div class="flex justify-center">
+  <div class="mb-3 xl:w-96">
+    <select class="form-select appearance-none
+      block
+      w-full
+      px-3
+      mt-4
+      py-1.5
+      text-base
+      font-normal
+      text-gray-400
+      bg-gray-100 bg-clip-padding bg-no-repeat
+      border border-solid border-gray-300
+      rounded
+      transition
+      ease-in-out
+      m-0
+      focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" aria-label="Default select example">
+        <option selected>Country</option>
+        <option value="1">USA</option>
+        <option value="2">Canada</option>
+        <option value="3">UK</option>
+    </select>
+  </div>
+</div>
+  <div class="">
+       <input
+          type="text"
+          name="zip_code"
+          class="px-4 py-3 mt-4 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm"
+          placeholder="Zip Code"
+      />
+    </div>
+<button
           type="#"
-          class="mt-4 px-4 py-3  leading-6 text-base rounded-md border border-transparent text-white bg-blue-500  hover:text-white focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 cursor-pointer inline-flex w-full justify-center items-center font-medium focus:outline-none"
+          class="mt-4 px-4 py-3  leading-6 text-base rounded-md border border-transparent text-white bg-gray-900 hover:text-white focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 cursor-pointer inline-flex w-full justify-center items-center font-medium focus:outline-none"
       >
         Register
       </button>
