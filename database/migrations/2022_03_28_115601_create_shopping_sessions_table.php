1d43e6c36d02c0de11287fa16e2d009eb364bd6d
MuhammedSultan1 <mfsultan01@gmail.com>
2022-03-28 11:34:59 -0400

    added shopping sessions migration

---
+++ database/migrations/2022_03_28_115601_create_shopping_sessions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shopping_sessions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->decimal('total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shopping_sessions');
+    }
+};
