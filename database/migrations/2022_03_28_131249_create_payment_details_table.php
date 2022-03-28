91a37ab02133f4d103c2ac2b2486460dcd14a42a
MuhammedSultan1 <mfsultan01@gmail.com>
2022-03-28 11:35:28 -0400

    added payment details migration

---
+++ database/migrations/2022_03_28_131249_create_payment_details_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payment_details', function (Blueprint $table) {
+            $table->id();
+            $table->int('order_id');
+            $table->int('amount');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payment_details');
+    }
+};
