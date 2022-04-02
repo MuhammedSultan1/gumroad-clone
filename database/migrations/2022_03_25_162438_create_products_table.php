3ff18602d7da4e7b3886d2655131b2afb682e659
MuhammedSultan1 <mfsultan01@gmail.com>
2022-04-02 15:47:14 -0400

    added the "number_available" field

--- database/migrations/2022_03_25_162438_create_products_table.php
+++ database/migrations/2022_03_25_162438_create_products_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->string('cover_photo');
             $table->string('description');
             $table->bigInteger('price');
+            $table->unsignedBigInteger('number_available');
             $table->timestamps();
         });
     }
