2c01e8a5ef5889f75a14068d08d9b41262168e81
MuhammedSultan1 <mfsultan01@gmail.com>
2022-04-02 15:18:35 -0400

    added UserPayment model and the "user payments" migration

---
+++ database/migrations/2022_04_02_191249_create_user_payments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_payments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('payment_type');
+            $table->string('provider');
+            $table->unsignedBigInteger('account_no');
+            $table->date('expiry');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_payments');
+    }
+};
